e23fd149643c1315fa253930446e9fa2aa93c6a2
Fijavz <squeak.kissy@gmail.com>
2025-09-27 21:07:55 +0200

    added kernel loading :P

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -28,8 +28,7 @@ step2:
   mov eax, cr0
   or eax, 0x1
   mov cr0, eax
-  ;jmp CODE_SEG:load32
-  jmp $
+  jmp CODE_SEG:load32
   
 ;GDT
 gdt_start:
@@ -60,6 +59,74 @@ gdt_end:
 gdt_descriptor:
   dw gdt_end - gdt_start-1
   dd gdt_start 
+
+[BITS 32]
+load32:
+  mov eax, 1
+  mov ecx, 100
+  mov edi, 0x0100000
+  call ata_lba_read
+  jmp CODE_SEG:0x0100000 ; 5 zeros
+
+
+ata_lba_read:
+  mov ebx, eax, ;backup LBA
+  ;Send the highest 8 bits of the lva to hard disk controller
+  shr eax, 24
+  or eax, 0xE0 ; Selects the master drive
+  mov dx, 0x1F6
+  out dx, al
+  ; Finished sending highest 8 bits of the lba
+
+  ; Send the total sectors to read
+  mov eax, ecx
+  mov dx, 0x1F2
+  out dx, al
+  ; Finished sending total sectors to read
+
+  ;Send more bits of the LBA
+  mov eax, ebx; Resotre backup LBA
+  mov dx, 0x1F3
+  out dx, al
+  ; Finished sending more bits of the LBA
+
+  ;Send more bits to the LBA
+  mov dx, 0x1F4
+  mov eax, ebx ; Redundant, resotring backup LBA, will continue this scheme of backing up
+  shr eax, 8
+  out dx, al 
+  ; Finished sending more bit sof the LBA
+
+  ; Send upper 16 bits of the LBA
+  mov dx, 0x1F5
+  mov eax, ebx ; Restore backup LBA
+  shr eax, 16
+  out dx, al
+  ;Finished sending upper 16 bits of the LBA
+
+  mov dx, 0x1F7
+  mov al, 0x20
+  out dx, al
+
+  ; Read all sectors into memory
+.next_sector:
+  push ecx
+
+  ; Checking if we need to read
+.try_again:
+  mov dx, 0x1F7
+  in al, dx
+  test al, 8
+  jz .try_again
+
+; We need to read 256 words at once
+  mov ecx, 256
+  mov dx, 0x1F0
+  rep insw
+  pop ecx
+  loop .next_sector
+  ; End of reading sectors into memory
+  ret
 
 times 510-($ - $$) db 0
 dw 0xAA55
--- src/kernel.asm
+++ src/kernel.asm
@@ -1,7 +1,10 @@
 [BITS 32]
+
+global _start
+
 CODE_SEG equ 0x08
 DATA_SEG equ 0x10
-load32:
+_start:
   mov ax, DATA_SEG
   mov ds, ax
   mov es, ax
@@ -10,8 +13,10 @@ load32:
   mov ss, ax
   mov ebp, 0x00200000
   mov esp, ebp
+
   ;Enable A20 line (super fast way, no loops, no polling)
   in al, 0x92
   or al, 2
   out 0x92, al
+
   jmp $
